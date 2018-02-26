d8f7ecd01167c58895497003836e6075c894d1e7
Emperorlou <nikolasarmstrong@gmail.com>
2018-02-26 16:16:54 -0500

    First commit, not working yet though. For some reason the servlet cannot start the exe..?

---
+++ src/com/journaldev/servlet/Test.java
@@ -0,0 +1,46 @@
+package com.journaldev.servlet;
+
+import java.io.IOException;
+
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.ExecuteException;
+
+public class Test
+{
+
+	public Test()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		String line = "C:\\Initium\\ImageProcessorServer\\webapps\\process\\WEB-INF\\kittify.exe";
+		CommandLine cmdLine = new CommandLine(line);
+		cmdLine.addArgument("C:\\Users\\Owner\\Google Drive\\Initium\\Asset Collaboration\\New Icon Art\\Ava\\weird-green-thing1.png");
+		
+		DefaultExecutor executor = new DefaultExecutor();
+		System.out.println("Executing "+cmdLine.toString());
+		executor.setExitValue(1);			
+		int exitValue=-100;
+		try
+		{
+			exitValue = executor.execute(cmdLine);
+		}
+		catch (ExecuteException e)
+		{
+			e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Exit value: "+exitValue);
+	}
+
+}
